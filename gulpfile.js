bbf2b3e62ab406f611f046c790b244e5be2ad935
Vinh <vjhnguyen@gmail.com>
2021-04-28 12:32:04 +0700

    gulp setup project

---
+++ gulpfile.js
@@ -0,0 +1,67 @@
+const { series } = require('gulp');
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+var cssnano = require('gulp-cssnano');
+var imagemin = require('gulp-imagemin');
+var cache = require('gulp-cache');
+var del = require('del');
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    },
+  })
+})
+
+gulp.task('sass', function(){
+  return gulp.src('app/scss/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+gulp.task('useref', function(){
+  return gulp.src('app/*.html')
+    .pipe(useref())
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulp.dest('dist'))
+});
+
+gulp.task('images', function(){
+  return gulp.src('app/images/**/*.+(png|jpg|jpeg|gif|svg)')
+  .pipe(cache(imagemin({
+      interlaced: true
+    })))
+  .pipe(gulp.dest('dist/images'))
+});
+
+gulp.task('fonts', function() {
+  return gulp.src('app/fonts/**/*')
+  .pipe(gulp.dest('dist/fonts'))
+})
+
+gulp.task('clean:dist', function(cb) {
+  del.sync('dist');
+  cb();
+})
+
+
+gulp.task('watch', gulp.parallel('browserSync','sass', function (){
+  gulp.watch('app/scss/**/*.scss', gulp.series('sass')); 
+  gulp.watch('app/*.html', browserSync.reload); 
+  gulp.watch('app/js/**/*.js', browserSync.reload); 
+}));
+
+gulp.task('build', gulp.series(`clean:dist`, `sass`, `useref`, `images`, `fonts`), function (){
+  console.log('Building files');
+})
+
+exports.default = series('watch')
