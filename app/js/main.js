81129e2356f593836e6320410f1c1ab4a1b1bf4f
Vinh <vjhnguyen@gmail.com>
2022-02-17 04:51:23 +0700

    add full page - demo

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,11 @@
-$(document).ready(function(){
-  AOS.init();
-
-  const menuBtn = $('.menuBtn')
-  const menuHeader = $('#menuHeader')
-
-  menuBtn.click(()=>{
-    menuHeader.toggleClass('hidden')
-    menuBtn.toggleClass('active')
-  })
+var myFullpage = new fullpage('#fullpage', {
+  anchors: ['firstPage', 'secondPage', '3rdPage'],
+  sectionsColor: ['#C63D0F', '#1BBC9B', '#7E8F7C'],
+  navigation: true,
+  navigationPosition: 'right',
+  navigationTooltips: ['First page', 'Second page', 'Third and last page']
 });
-		
+
+$(document).ready(function() {
+
+});
